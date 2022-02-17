4439bc5ac5c00de5988a8f22f84b916f8256c526
Макс Царьов <makstsar1701@gmail.com>
2022-02-17 13:57:42 +0200

    Add endpoints for tasks

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from enum import Enum
 
 db = SQLAlchemy()
 
@@ -11,9 +12,11 @@ class UserModel(db.Model):
     email = db.Column(db.String(64), unique=True, nullable=False)
     _pwd = db.Column(db.String(225), nullable=False)
 
-    def __init__(self, email, pwd):
-        self.email = email
-        self._pwd = pwd
+    def __init__(self, **kwargs):
+        super(UserModel, self).__init__(**kwargs)
+
+    def get_pwd(self):
+        return self._pwd
 
     @classmethod
     def getUsers(cls):
@@ -21,18 +24,19 @@ class UserModel(db.Model):
             return {
                 "id": user_object.id,
                 "email": user_object.email,
-                "password": user_object._pwd
+                "password": user_object.get_pwd()
             }
+
         return [to_json(i) for i in cls.query.all()]
 
     @classmethod
-    def getUser(cls, id):
-        return cls.query.filter_by(id=id).first()
+    def getUser(cls, user_id):
+        return cls.query.filter_by(id=user_id).first()
 
     @classmethod
     def addUser(cls, email, password):
         try:
-            user = cls(email, generate_password_hash(password))
+            user = cls(email=email, _pwd=generate_password_hash(password))
             db.session.add(user)
             db.session.commit()
             return True
@@ -54,3 +58,234 @@ class UserModel(db.Model):
     @staticmethod
     def verify_password(password_hash, password):
         return check_password_hash(password_hash, password)
+
+
+class RepeatType(Enum):
+    DAILY = 'daily'
+    WEEKLY = 'weekly'
+    MONTHLY = 'monthly'
+
+
+class TasksModel(db.Model):
+    __tablename__ = "task"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    deadline = db.Column(db.DateTime)
+    repeat = db.Column(db.Enum(RepeatType))
+    suff_time = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+    status = db.Column(db.Boolean, nullable=False)
+
+    def __init__(self, **kwargs):
+        super(TasksModel, self).__init__(**kwargs)
+
+    @classmethod
+    def add_task(cls, user_id, **kwargs):
+        try:
+            task = cls(user_id=user_id, status=False, **kwargs)
+            db.session.add(task)
+            db.session.commit()
+            return True
+
+        except Exception as e:
+            print(e)
+            return False
+
+    @classmethod
+    def get_all_tasks(cls, user_id):
+        def to_json(task):
+            return {"title": task.title,
+                    "deadline": task.deadline,
+                    "repeat": task.repeat.name if task.repeat else None,
+                    "suff_time": task.suff_time,
+                    "notes": task.notes,
+                    "status": task.status,
+                    "subtasks": SubTaskModel.get_subtasks(task.id),
+                    "tags": TagsModel.get_tags(task.id)}
+
+        tasks = cls.query.filter_by(user_id=user_id)
+        return {str(task.id): to_json(task) for task in tasks}
+
+    @classmethod
+    def get_last_task_id(cls):
+        tasks = cls.query.all()
+        max_id = 0
+        for t in tasks:
+            if t.id > max_id:
+                max_id = t.id
+
+        return max_id
+
+    @classmethod
+    def delete_task(cls, task_id):
+        task = cls.query.filter_by(id=task_id).first()
+
+        if not task:
+            return {"error": "Not existing task_id"}
+
+        TagsModel.delete_tags(task_id)
+        SubTaskModel.delete_subtasks(task_id)
+
+        db.session.delete(task)
+        db.session.commit()
+
+        return {"success": True}
+
+    @classmethod
+    def update_task(cls, task_id, **kwargs):
+        task = cls.query.filter_by(id=task_id)
+
+        if not task.first():
+            return {"error": "Not existing task", "status_code": 400}
+        else:
+            task.update(kwargs)
+            try:
+                db.session.commit()
+                return {"success": True}
+            except Exception as e:
+                print("Failed to change task data: ", e)
+                return {"error": str(e), "status_code": 500}
+
+
+class SubTaskModel(db.Model):
+    __tablename__ = "subtask"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    task_id = db.Column(db.Integer, db.ForeignKey("task.id"), primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    status = db.Column(db.Boolean, nullable=False)
+
+    def __init__(self, **kwargs):
+        super(SubTaskModel, self).__init__(**kwargs)
+
+    @classmethod
+    def add_subtasks(cls, task_id, *args):
+        try:
+            for title in args:
+                subtask = cls(task_id=task_id, title=title, status=False)
+                db.session.add(subtask)
+                db.session.commit()
+            return True
+
+        except Exception as e:
+            print(e)
+            return False
+
+    @classmethod
+    def get_subtasks(cls, task_id):
+        return [{"title": subtask.title, "status": subtask.status}
+                for subtask in cls.query.filter_by(task_id=task_id)]
+
+    @classmethod
+    def delete_subtasks(cls, task_id):
+        for subtask in cls.query.filter_by(task_id=task_id).all():
+            db.session.delete(subtask)
+        db.session.commit()
+
+    @classmethod
+    def delete_subtask_by_title(cls, task_id, title):
+        subtask = cls.query.filter_by(task_id=task_id, title=title).first()
+
+        if not subtask:
+            return {"error": "Not existing subtask", "status_code": 400}
+
+        try:
+            db.session.delete(subtask)
+            db.session.commit()
+            return {"success": True, "status_code": 200}
+        except Exception as e:
+            return {"error": str(e), "status_code": 500}
+
+    @classmethod
+    def update_subtask(cls, task_id, title, **kwargs):
+        subtask = cls.query.filter_by(task_id=task_id, title=title)
+
+        if not subtask.first():
+            return {"error": "Not existing task", "status_code": 400}
+        else:
+            if "new_title" in kwargs:
+                kwargs["title"] = kwargs.pop("new_title")
+            subtask.update(kwargs)
+            try:
+                db.session.commit()
+                return {"success": True}
+            except Exception as e:
+                print("Failed to change task data: ", e)
+                return {"error": str(e), "status_code": 500}
+
+
+class TagsModel(db.Model):
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    task_id = db.Column(db.Integer, db.ForeignKey("task.id"), primary_key=True)
+    tag_title = db.Column(db.String(50))
+
+    def __init__(self, **kwargs):
+        super(TagsModel, self).__init__(**kwargs)
+
+    @classmethod
+    def add_tag(cls, task_id, tag_title):
+        try:
+            tag = cls(task_id=task_id, tag_title=tag_title)
+            db.session.add(tag)
+            db.session.commit()
+            return True
+
+        except Exception as e:
+            print(e)
+            return False
+
+    @classmethod
+    def add_tags(cls, task_id, *args):
+        try:
+            for title in args:
+                tag = cls(task_id=task_id, tag_title=title)
+                db.session.add(tag)
+                db.session.commit()
+            return True
+
+        except Exception as e:
+            print(e)
+            return False
+
+    @classmethod
+    def get_tags(cls, task_id):
+        return [tag.tag_title for tag in cls.query.filter_by(task_id=task_id)]
+
+    @classmethod
+    def delete_tags(cls, task_id):
+        for tag in cls.query.filter_by(task_id=task_id).all():
+            db.session.delete(tag)
+        db.session.commit()
+
+    @classmethod
+    def delete_tag_by_title(cls, task_id, tag_title):
+        tag = cls.query.filter_by(task_id=task_id, tag_title=tag_title).first()
+
+        if not tag:
+            return {"error": "Not existing tag", "status_code": 400}
+
+        try:
+            db.session.delete(tag)
+            db.session.commit()
+            return {"success": True, "status_code": 200}
+        except Exception as e:
+            return {"error": str(e), "status_code": 500}
+
+    @classmethod
+    def update_tag(cls, task_id, tag_title, new_title):
+        tag = cls.query.filter_by(task_id=task_id, tag_title=tag_title).first()
+
+        if not tag:
+            return {"error": "Not existing tag", "status_code": 400}
+
+        tag.tag_title = new_title
+
+        try:
+            db.session.commit()
+            return {"success": True, "status_code": 200}
+        except Exception as e:
+            return {"error": str(e), "status_code": 500}
---
+++ app/tasks.py
@@ -0,0 +1,176 @@
+
+from flask import request, jsonify, Blueprint
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from .models import TasksModel, SubTaskModel, TagsModel
+
+tasks_manager = Blueprint('tasks', __name__, url_prefix='/tasks')
+
+
+@tasks_manager.route('/get_all_tasks', methods=["GET"])
+@jwt_required()
+def get_all_tasks():
+    current_user = get_jwt_identity()
+    tasks = TasksModel.get_all_tasks(current_user)
+    tasks["success"] = True
+
+    return jsonify(tasks), 200
+
+
+@tasks_manager.route('/add_task', methods=["POST"])
+@jwt_required()
+def add_task():
+    current_user = get_jwt_identity()
+
+    request_json = request.get_json()
+    subtasks = None
+    tags = None
+
+    if "title" not in request_json:
+        return jsonify({"error": "Invalid form"}), 400
+    if "subtasks" in request_json:
+        subtasks = request_json.pop("subtasks")
+    if "tags" in request_json:
+        tags = request_json.pop("tags")
+
+    add_task_res = TasksModel.add_task(current_user, **request_json)
+    if not add_task_res:
+        return jsonify({"error": "Can't add tasks to database"}), 500
+
+    task_id = TasksModel.get_last_task_id()
+    if subtasks:
+        if isinstance(subtasks, list):
+            add_subtask_res = SubTaskModel.add_subtasks(task_id, *subtasks)
+        else:
+            add_subtask_res = SubTaskModel.add_subtasks(task_id, subtasks)
+
+        if not add_subtask_res:
+            return jsonify({"error": "Can't add subtasks to database"}), 500
+
+    if tags:
+        if isinstance(tags, list):
+            add_tag_res = TagsModel.add_tags(task_id, *tags)
+        else:
+            add_tag_res = TagsModel.add_tags(task_id, tags)
+
+        if not add_tag_res:
+            return jsonify({"error": "Can't add tags to database"}), 500
+
+    return jsonify({"success": True}), 200
+
+
+@tasks_manager.route('/delete_task', methods=["POST"])
+@jwt_required()
+def delete_task():
+    if "task_id" not in request.json:
+        return jsonify({"error": "Invalid form"}), 400
+    result = TasksModel.delete_task(request.json["task_id"])
+
+    if "success" in result:
+        return jsonify(result), 200
+    else:
+        return jsonify(result), 400
+
+
+@tasks_manager.route('/update_task', methods=["POST"])
+@jwt_required()
+def update_task():
+    request_json = request.get_json()
+
+    if "task_id" not in request_json:
+        return jsonify({"error": "Invalid form"}), 400
+
+    task_id = request_json.pop("task_id")
+    result = TasksModel.update_task(task_id, **request_json)
+
+    if "error" in result:
+        return jsonify({"error": result["error"]}), result["status_code"]
+    else:
+        return jsonify({"success": result["success"]}), 200
+
+
+@tasks_manager.route('/add_tags', methods=["POST"])
+@jwt_required()
+def add_tags():
+    if "task_id" not in request.json or "tags" not in request.json:
+        return jsonify({"error": "Invalid form"}), 400
+
+    result = TagsModel.add_tags(request.json["task_id"], *request.json["tags"])
+
+    if result:
+        return jsonify({"success": True}), 200
+    else:
+        return jsonify({"error": "Failed to add tags"}), 500
+
+
+@tasks_manager.route('/update_tag', methods=["POST"])
+@jwt_required()
+def update_tag():
+    if "task_id" not in request.json or "tag_title" not in request.json or "new_title" not in request.json:
+        return jsonify({"error": "Invalid form"}), 400
+
+    result = TagsModel.update_tag(request.json["task_id"], request.json["tag_title"], request.json["new_title"])
+
+    if "success" in result:
+        return jsonify({"success": result["success"]}), result["status_code"]
+    else:
+        return jsonify({"error": result["error"]}), result["status_code"]
+
+
+@tasks_manager.route('/delete_tag', methods=["POST"])
+@jwt_required()
+def delete_tag():
+    if "task_id" not in request.json or "tag_title" not in request.json:
+        return jsonify({"error": "Invalid form"}), 400
+
+    result = TagsModel.delete_tag_by_title(request.json["task_id"], request.json["tag_title"])
+
+    if "success" in result:
+        return jsonify({"success": result["success"]}), result["status_code"]
+    else:
+        return jsonify({"error": result["error"]}), result["status_code"]
+
+
+@tasks_manager.route('/add_subtasks', methods=["POST"])
+@jwt_required()
+def add_subtasks():
+    if "task_id" not in request.json or "subtasks" not in request.json:
+        return jsonify({"error": "Invalid form"}), 400
+
+    result = SubTaskModel.add_subtasks(request.json["task_id"], *request.json["subtasks"])
+
+    if result:
+        return jsonify({"success": True}), 200
+    else:
+        return jsonify({"error": "Failed to add subtasks"}), 500
+
+
+@tasks_manager.route('/update_subtask', methods=["POST"])
+@jwt_required()
+def update_subtask():
+    request_json = request.get_json()
+
+    if "task_id" not in request_json or "title" not in request_json:
+        return jsonify({"error": "Invalid form"}), 400
+
+    task_id = request_json.pop("task_id")
+    title = request_json.pop("title")
+    result = SubTaskModel.update_subtask(task_id, title, **request_json)
+
+    if "error" in result:
+        return jsonify({"error": result["error"]}), result["status_code"]
+    else:
+        return jsonify({"success": result["success"]}), 200
+
+
+@tasks_manager.route('/delete_subtask', methods=["POST"])
+@jwt_required()
+def delete_subtask():
+    if "task_id" not in request.json or "title" not in request.json:
+        return jsonify({"error": "Invalid form"}), 400
+
+    result = SubTaskModel.delete_subtask_by_title(request.json["task_id"], request.json["title"])
+
+    if "success" in result:
+        return jsonify({"success": result["success"]}), result["status_code"]
+    else:
+        return jsonify({"error": result["error"]}), result["status_code"]
